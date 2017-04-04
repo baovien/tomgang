e1e05bd6e9660ef2b5a84acb96b8db3664c36910
Kristoffer Myhren <kristm15@uia.no>
2017-04-04 10:29:35 +0200

    Rask fix på numbers

--- tomgang/wwwroot/js/site.js
+++ tomgang/wwwroot/js/site.js
@@ -15,19 +15,19 @@ function timer(){
     }
 
     gainCount += autoClick/5;
-    gainCount += proteins/2;
+    gainCount += protein/2;
     gainCount += stringlet;
     gainCount += labCount*2;
     update();
 }
 
 function update(){
-    $('#gainNumber').html(gainCount); 
+    $('#gainNumber').html(gainCount.toFixed(2)); 
     $('#amountAutoClick').html("You Own " + autoClick + " Carb snack(s)");
     $('#costAutoClick').html("Cost: " + ((autoClick+1) * 12));
 
-    $('#amountProteins').html("You Own " + proteins + " Protein shake(s)");
-    $('#costProteins').html("Cost: " + ((proteins+1) * 30));
+    $('#amountProtein').html("You Own " + protein + " Protein shake(s)");
+    $('#costProtein').html("Cost: " + ((protein+1) * 30));
 
     $('#amountStringlet').html("You Own " + stringlet + " Stringlet(s)");
     $('#costStringlet').html("Cost: " + ((stringlet+1) * 60));
@@ -35,33 +35,43 @@ function update(){
     $('#amountLab').html("You Own " + labCount + " Grunt(s)");
     $('#costLab').html("Cost: " + ((labCount+1) * 120));
 
-    $('#gainspersecond').html((((autoClick)+(labCount*2))*multiplier) + " Gains/s");
+    $('#gainspersecond').html((((autoClick/5)+(protein/2)+(stringlet)+(labCount*2))*multiplier).toFixed(2) + " Gains/s");
 }
 
 
 function add(){
     gainCount += 1;
-     $('#gainNumber').html(gainCount); 
+     $('#gainNumber').html(gainCount.toFixed(2)); 
 }
 
 function save(){
     localStorage.setItem("gaincount", gainCount);
     localStorage.setItem("autoclick", autoClick);
+    localStorage.setItem("protein", autoClick);
+    localStorage.setItem("stringlet", autoClick);
     localStorage.setItem("lab", labCount);
 }
 
 function load(){
     gainCount = localStorage.getItem("gaincount");
-    gainCount = parseInt(gainCount);
+    gainCount = parseDouble(gainCount);
+
     autoClick = localStorage.getItem("autoclick");
-    autoClick = parseInt(autoClick);
+    autoClick = parseDouble(autoClick);
+
+    protein = localStorage.getItem("protein");
+    protein = parseDouble(protein);
+
+    stringlet = localStorage.getItem("stringlet");
+    stringlet = parseDouble(stringlet);
+
     labCount = localStorage.getItem("lab");
     labCount = parseInt(labCount);
     update();
 }
 
 //UPGRADES
-//Carbs value per = 0.2
+//Carbs, value per = 0.2
 function buyCarbs() {
     if(gainCount >= ((autoClick + 1) * 12)){
         gainCount -= ((autoClick + 1) * 12);
@@ -69,15 +79,15 @@ function buyCarbs() {
         update();
     }
 }
-//Proteins value per = 0.5
-function buyProteins() {
-    if(gainCount >= ((proteins + 1) * 30)){
-        gainCount -= ((proteins + 1) * 30);
-        proteins += 1;
+//Proteins, value per = 0.5
+function buyProtein() {
+    if(gainCount >= ((protein + 1) * 30)){
+        gainCount -= ((protein + 1) * 30);
+        protein += 1;
         update();
     }
 }
-//Stringlets value per = 1
+//Stringlets, value per = 1
 function buyStringlet() {
     if(gainCount >= ((stringlet + 1) * 60)){
         gainCount -= ((stringlet + 1) * 60);
@@ -85,7 +95,7 @@ function buyStringlet() {
         update();
     }
 }
-//Labs "Grunts på view" value per = 2
+//Labs "Grunts på view", value per = 2
 function buyLab() {
     if(gainCount >= ((labCount + 1) * 120)){
         gainCount -= ((labCount+1) * 120);
@@ -98,7 +108,7 @@ $(document).ready(function() {
   gainCount = 0;
   autoClick = 0;
   labCount = 0;
-  proteins = 0;
+  protein = 0;
   stringlet = 0;
   multiplier = 1;
   setInterval(timer, 1000);
