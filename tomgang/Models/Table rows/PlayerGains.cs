ef7d61994f792971bed2745f4d20fb5fbdfd71fb
Anders Ghouchbar <anderg15@student.uia.no>
2017-05-18 21:35:13 +0200

    Adda litt values til PlayerGains

--- tomgang/Models/Table rows/PlayerGains.cs
+++ tomgang/Models/Table rows/PlayerGains.cs
@@ -4,13 +4,17 @@ namespace tomgang.Models
 {
     public class PlayerGains : IPlayerGains
     {
-        public PlayerGains(string id, int val){
+        public PlayerGains(string id, int currentGains, int income, int click){
             Id = id;
-            value = val;
+            currentGainsValue = currentGains;
+            incomeValue = income;
+            clickValue = click;
         }
         [Key]
         public string Id{get; set;}
 
-        public int value{get; set;}
+        public int clickValue{get; set;}
+        public int incomeValue{get; set;}
+        public int currentGainsValue{get; set;}
     }
 }
