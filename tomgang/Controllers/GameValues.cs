ae9118763c1a3308251d2f94526828660ab22ead
Anders Ghouchbar <anderg15@student.uia.no>
2017-05-20 13:44:11 +0200

    Adda funksjonalitet i gamevalues

--- tomgang/Controllers/GameValues.cs
+++ tomgang/Controllers/GameValues.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Linq;
 using tomgang.Services;
 using tomgang.Data;
 using tomgang.Models;
@@ -36,18 +37,60 @@ namespace tomgang.Controllers
             //Getter Gains/s til brukeren
             //Adder Gains/s på current Gains utifra hvor lang tid som har gått siden sist
         }
-        public void buyUpgrade(){
+
+
+        //Denne trenger userid, typen upgrade kjøpt og tilhørende value
+        public void buyUpgrade(string userid, int id, double value){
+            //IDen bestemmer hva upgraden vil påvirke. Om flere typer
+            //upgrades er ønsket er det bare å adde en case for typen.
             
-            _dbContext.SaveChanges();
             //Henter upgraden som er kjøpt og applyer den til brukeren
             //Setter den som kjøpt
-        }
-        public void checkAchievements(){
-
+            switch (id){
+                case 1:
+                    //Multipliser med income
+                    _dbContext.PlayerGains.Find(userid).incomeValue *= value;
+                    break;
+                case 2:
+                    //Add på income
+                    _dbContext.PlayerGains.Find(userid).incomeValue += value;
+                    break;
+                case 3:
+                    _dbContext.PlayerGains.Find(userid).clickValue *= value;
+                    //Multipliser med click
+                    break;
+                case 4:
+                    _dbContext.PlayerGains.Find(userid).clickValue += value;
+                    //Add på click
+                    break;
+            }
+            _dbContext.PlayerAchievments.Add(new PlayerAchievements(userid, id));
             _dbContext.SaveChanges();
         }
-        public void giveAchievment(){
-
+        public void checkAchievements(string userid){
+                //Loope igjennom alle achievement reqs og sjekke om brukeren har fått noen
+                //Adde achievments brukeren har fått
+        
+                for (int i = 0; i < _dbContext.Achievement.Count(); i++)
+                {
+                    switch(_dbContext.Achievement.Find(i).type){
+                        case 0:
+                        //Sjekker user har større verdi enn required og om user har achien fra før
+                            if(_dbContext.PlayerGains.Find(userid).currentGainsValue >= 
+                            _dbContext.Achievement.Find(i).value &&
+                            _dbContext.PlayerAchievments.Find(userid, i) != null){
+                                _dbContext.PlayerAchievments.Add(new PlayerAchievements(userid, i));
+                            }
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                            
+                    }
+                }
+                    
+                }
             _dbContext.SaveChanges();
         }
     }
