e01f4d1efdbf15167b9dcb8e1723994cdc76672f
Vien Bao Ngo <vienbn15@uia.no>
2017-05-30 02:11:49 +0200

    layout oppdatering

--- tomgang/wwwroot/js/site.js
+++ tomgang/wwwroot/js/site.js
@@ -1,53 +1,60 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	//Vise popover info for upgradsa
-	$('.upgradeImg').popover();
-	$('.itemImg').popover();
+  //Vise popover info for upgradsa
+  $('.upgradeImg').popover();
+  $('.itemImg').popover();
 
-	//Innlogging/registrering
-	$('#login-form-link').click(function (e) {
-		$("#login-form").delay(100).fadeIn(100);
-		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function (e) {
-		$("#register-form").delay(100).fadeIn(100);
-		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+	//MAIN
+  $(".btn-pref .btn").click(function() {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below
+    $(this).removeClass("btn-default").addClass("btn-primary");
+  });
 
-	$("#login-dp").click(function(e){
-  		e.stopPropagation();
-	});
+  //Innlogging/registrering
+  $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
 
-	$("#letsgoleft").click(function(){
-  		$("#testlols").load("Manage/ChangePassword", $('#testlols').serialize());
-	});
+  $("#login-dp").click(function(e) {
+    e.stopPropagation();
+  });
+
+  $("#letsgoleft").click(function() {
+    $("#testlols").load("Manage/ChangePassword", $('#testlols').serialize());
+  });
 });
 
 function initialize() {
-	//Connecter til chathub
-	chatHub();
+  //Connecter til chathub
+  chatHub();
 
-	//Spillfunksjoner
-	liftClickPost();
-	upgradeBtnsPost();
-	itemBtnsPost();
+  //Spillfunksjoner
+  liftClickPost();
+  upgradeBtnsPost();
+  itemBtnsPost();
 
 }
 
 function update() { //Kjøres i timer funksjon i index.
-	//Oppdaterer current gains
-	getEligibleUpgrades();
-	getCurrentGains();
+  //Oppdaterer current gains
+  getEligibleUpgrades();
+  getCurrentGains();
 
-	increaseGains();
-	updateItemsCost();
-	updateUpgradesStatus();
-	updateItemsStatus();
-	updateGainsCounter();
+  increaseGains();
+  updateItemsCost();
+  updateUpgradesStatus();
+  updateItemsStatus();
+  updateGainsCounter();
 }
