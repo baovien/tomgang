d4da5c2b8fee7334c2040b55b4cab8afd04d9e75
Anders Ghouchbar <anderg15@student.uia.no>
2017-05-30 03:12:41 +0200

    adda flere upgrades og achis

--- tomgang/Models/Table rows/Achievement.cs
+++ tomgang/Models/Table rows/Achievement.cs
@@ -10,19 +10,20 @@ namespace tomgang.Models
 
         public Achievement () {}
 
-        public Achievement(string id, int typ, int val, string ImgPath){
+        public Achievement(string id, int typ, int val, string ImgPath, string desc){
             Id = id;
             type = typ;
             value = val;
             imgPath = ImgPath;
+            description = desc;
         }
 
         [Key]
         public string Id{get; set;}
-
         public int type{get; set;}
         public int value{get; set;}
         public string imgPath{get;set;}
+        public string description{get;set;}
     }
     public class Achievements{
         public IList<Achievement> achievements {get;set;}
